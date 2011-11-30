490e4ef9d7b3bb53c19e59016a2e28bc266c9128
chr.lerche <chr.lerche@googlemail.com>
2011-11-30 09:39:47 +0100

    added a max age default constant

--- ws4d-jcoap/src/org/ws4d/coap/Constants.java
+++ ws4d-jcoap/src/org/ws4d/coap/Constants.java
@@ -19,6 +19,7 @@ public final class Constants {
     public final static int MESSAGE_ID_MIN = 0;
     public final static int MESSAGE_ID_MAX = 65535;
     public final static int COAP_MESSAGE_SIZE_MAX = 1152;
-
     public final static int COAP_DEFAULT_PORT = 5683;
+    public final static int COAP_DEFAULT_MAX_AGE_S = 60;
+    public final static int COAP_DEFAULT_MAX_AGE_MS = COAP_DEFAULT_MAX_AGE_S * 1000;
 }
