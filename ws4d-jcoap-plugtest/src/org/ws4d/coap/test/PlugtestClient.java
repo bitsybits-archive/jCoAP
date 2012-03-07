206a1593749bd0fff1280b56889a229d531f35d7
Nico Laum <nico.laum@uni-rostock.de>
2012-03-07 09:47:18 +0100

    Plugtest server and client

---
+++ ws4d-jcoap-plugtest/src/org/ws4d/coap/test/PlugtestClient.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package org.ws4d.coap.test;
+
+/**
+ * @author Nico Laum <nico.laum@uni-rostock.de>
+ *
+ */
+public class PlugtestClient {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+	// TODO Auto-generated method stub
+
+    }
+
+}
---
+++ ws4d-jcoap-plugtest/src/org/ws4d/coap/test/PlugtestServer.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package org.ws4d.coap.test;
+
+/**
+ * @author Nico Laum <nico.laum@uni-rostock.de>
+ *
+ */
+public class PlugtestServer {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+	// TODO Auto-generated method stub
+
+    }
+
+}
